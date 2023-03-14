db4905f33065e9eb08c45bd537db1c19e44cc329
Miguel Freitas <miguelfreitas15012503@gmail.com>
2023-03-14 08:03:44 -0300

    Animes

---
+++ src/Anime.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class Anime {
+    public int Id;\
+    public String Nome;
+    public int Cadastro;
+    public LocalDate dataLAancamento;
+    public String Autor;
+    public String ClassificacaoIndicativa;
+    public int quantidadeEpisodios;
+    public int Temporadas;
+}
---
+++ src/AnimeTeste.java
@@ -0,0 +1,8 @@
+public class AnimeTeste {
+    public static void main(String[] args) {
+        Anime meuAnime = new Anime();
+        meuAnime.Nome = "As Bizarras Aventuras de Jalson e Otto juntos e ornintorrincos";
+        meuAnime.Temporadas = 3;
+        System.out.println(9);
+    }
+}
