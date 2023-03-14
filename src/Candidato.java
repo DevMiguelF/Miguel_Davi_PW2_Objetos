b3c4e0198f813a9ad6f796346b7b462195574928
Miguel Freitas <miguelfreitas15012503@gmail.com>
2023-03-14 07:58:22 -0300

    Initial Commit

---
+++ src/Candidato.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.time.LocalDate;
+
+public class Candidato {
+    public String nome;
+    public LocalDate DataDeNascimento;
+    public String Email;
+    public Number Celular;
+    public String Escolaridade;
+    public String Funcao;
+    public Float PretencaoSalarial;
+    public Checkbox Empregado;
+    public Checkbox Estagio;
+
+}
