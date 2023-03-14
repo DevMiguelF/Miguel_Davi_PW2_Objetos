16166e945c810e0acdaf1d72bb29b4f0791b052f
Miguel Freitas <miguelfreitas15012503@gmail.com>
2023-03-14 08:10:13 -0300

    Candidato Teste

--- src/Anime.java
+++ src/Anime.java
@@ -1,7 +1,7 @@
 import java.time.LocalDate;
 
 public class Anime {
-    public int Id;\
+    public int Id;
     public String Nome;
     public int Cadastro;
     public LocalDate dataLAancamento;
---
+++ src/CandidatoTeste.java
@@ -0,0 +1,8 @@
+public class CandidatoTeste {
+    public static void main(String[] args) {
+        Candidato MeuCandidato = new Candidato();
+        MeuCandidato.nome = "Jair Gosmes Lula";
+
+        System.out.println(MeuCandidato.nome);
+    }
+}
